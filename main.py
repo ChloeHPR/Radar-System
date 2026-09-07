5bdf3a2982a22310031c9187b037f7d28ef335f1
ChloeHPR <chloe.houpert@gmail.com>
2026-09-07 23:31:26 +0200

    Update radar file path for acquisition treatment

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ from speed_mode import acquisition_treatment
 
 def main():
     # path to the file for speed mode processing
-    fichier_triangle = "/Users/emiliahoupert/Downloads/RADAR_SPEED/Acquisition/acquistion_aller_retour_pause(2).wav" #
-    acquisition_treatment(fichier_triangle)
+    radar_file = "/Acquisition/acquistion_aller_retour_pause(2).wav" #
+    acquisition_treatment(radar_file)
 
 if __name__ == "__main__":
     main()
